095ba60adadaa9d6788be50196a86c7f3143de1c
pnm <michael.pantic@gmail.com>
2017-09-06 15:36:36 +0200

    Added simple attitude propagator (spin-axis), and extended all tools to include orientation

--- src/space_tf/Converter.py
+++ src/space_tf/Converter.py
@@ -1,6 +1,6 @@
 #   Converter between various earth centered coordinats
 #   Author: Michael Pantic, michael.pantic@gmail.com
-#	Author: Jean Noel Pittet  (Conversion Function ITRF-TEME)
+#    Author: Jean Noel Pittet  (Conversion Function ITRF-TEME)
 #   Author: Christophe Paccolat (original oe-tools)
 #   License: TBD
 
@@ -15,71 +15,85 @@ from Constants import *
 
 class Converter:
     # Generic main convert method
-	@staticmethod
-	def convert(source, target):
-		if isinstance(source, Cartesian) and isinstance(target, Cartesian):
-			Converter.convertCartesianToCartesian(source,target)
+    @staticmethod
+    def convert(source, target):
+        if isinstance(source, Cartesian) and isinstance(target, Cartesian):
+            Converter.convertCartesianToCartesian(source,target)
 
-		elif isinstance(source, Cartesian) and isinstance(target, SphericalEarth):
-		    # create itrf
-			itrf = CartesianITRF()
-		    #convert whatever cartesian frame to ITRF
-			Converter.convert(source, itrf)
-			Converter.convertITRFtoSpherical(itrf, target)
+        elif isinstance(source, Cartesian) and isinstance(target, SphericalEarth):
+            # create itrf
+            itrf = CartesianITRF()
+            #convert whatever cartesian frame to ITRF
+            Converter.convert(source, itrf)
+            Converter.convertITRFtoSpherical(itrf, target)
 
-		elif isinstance(source, OrbitalElements) and isinstance(target, Cartesian):
-		    source.toCartesian(target)
-		    target.frame = CartesianFrame.TEME
+        elif isinstance(source, OrbitalElements) and isinstance(target, Cartesian):
+            source.toCartesian(target)
+            target.frame = CartesianFrame.TEME
 
-		elif isinstance(source, Cartesian) and isinstance(target, OrbitalElements):
-		    # crate TEME Cartesian
-		    teme = CartesianTEME()
+        elif isinstance(source, Cartesian) and isinstance(target, OrbitalElements):
+            # crate TEME Cartesian
+            teme = CartesianTEME()
 
-		    # Convert whatever cartesian frame to TEME
-		    Converter.convert(source, teme)
+            # Convert whatever cartesian frame to TEME
+            Converter.convert(source, teme)
 
-		    # Init orbital elements from teme cartesian
-		    target.fromCartesian(teme)
+            # Init orbital elements from teme cartesian
+            target.fromCartesian(teme)
 
-		else:
-		    raise Exception("Not implemented conversion")
-
-
-	@staticmethod
-	def convertCartesianToCartesian(source,target):
-		if(source.frame == target.frame):
-			# copy all data to target object
-			target.__dict__.update(source.__dict__)
-
-		elif(source.frame == CartesianFrame.TEME and
-		target.frame == CartesianFrame.ITRF):
-			Converter.convertTEMEtoITRF(source, target)
-		else:
-			raise Exception("Not implemented cartesian conversion")
+        else:
+            raise Exception("Not implemented conversion")
 
 
-	@staticmethod
-	def convertTEMEtoITRF(source, target):
-		# from Code of previous student ....xy...
-		xp = 0.000
-		yp = 0.000
+    @staticmethod
+    def convertCartesianToCartesian(source,target):
+        if(source.frame == target.frame):
+            # copy all data to target object
+            target.__dict__.update(source.__dict__)
 
-		# TODO: Not sure if this converts correctly, as it does not
-		# incorporate the mean equinox direction?
+        elif(source.frame == CartesianFrame.TEME and
+        target.frame == CartesianFrame.ITRF):
+            Converter.convertTEMEtoITRF(source, target)
+        else:
+            raise Exception("Not implemented cartesian conversion")
 
-		# Rotate around z based on sidereal time in greenwich
-		gmst1982 = greenwichSiderealTime(target.epochJD)
-		R_gmst = R_z(gmst1982)
 
-		# Calculation rotation of polar motion (not in used at the moment)
-		W = R_x(-yp)*R_y(-xp)
+    @staticmethod
+    def convertTEMEtoITRF(source, target):
+        # from Code of previous student ....xy...
+        xp = 0.000
+        yp = 0.000
 
-		# Do rotations
-		target.R = R_gmst.T * W.T * source.R.reshape(3,1)
-		target.V = R_gmst.T * W.T * source.V.reshape(3,1)
-		target.epochJD = source.epochJD
+        # TODO: Not sure if this converts correctly, as it does not
+        # incorporate the mean equinox direction?
 
-	@staticmethod
-	def convertITRFtoSpherical(source, target):
-		target.lat = np.arctan(source.R[2] / np.sqrt(source.R[1] ** 2 + source.R[0] ** 2))[0,0]
-		target.lon = np.arctan2(source.R[1], source.R[0])[0,0]
+        # Rotate around z based on sidereal time in greenwich
+        gmst1982 = greenwichSiderealTime(target.epochJD)
+        R_gmst = R_z(gmst1982)
+
+        # Calculation rotation of polar motion (not in used at the moment)
+        W = R_x(-yp)*R_y(-xp)
+
+        # Do rotations
+        target.R = R_gmst.T * W.T * source.R.reshape(3,1)
+        target.V = R_gmst.T * W.T * source.V.reshape(3,1)
+        target.epochJD = source.epochJD
+
+    @staticmethod
+    def convertITRFtoSpherical(source, target):
+        target.lat = np.arctan(source.R[2] / np.sqrt(source.R[1] ** 2 + source.R[0] ** 2))[0,0]
+        target.lon = np.arctan2(source.R[1], source.R[0])[0,0]
+        
+    @staticmethod
+    def fromOEMessage(msg):
+        tf_obj = OrbitalElements()
+
+        tf_obj.i = np.deg2rad(msg.orbit.inclination)
+        tf_obj.w = np.deg2rad(msg.orbit.arg_perigee)
+        tf_obj.omega = np.deg2rad(msg.orbit.raan)
+        tf_obj.t = np.deg2rad(msg.orbit.true_anomaly)
+        tf_obj.a = msg.orbit.semimajoraxis
+        tf_obj.e = msg.orbit.eccentricity
+        return tf_obj
+
+
