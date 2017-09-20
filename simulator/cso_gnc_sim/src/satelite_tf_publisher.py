a9a7f2ba4007f21d707c0fba600023d05c60602d
pnm <michael.pantic@gmail.com>
2017-09-20 15:40:24 +0200

    added TF publisher configuration, texture, adjusted tf_publisher and rviz templates. NOTE: Texture still wronggit status

--- simulator/cso_gnc_sim/src/satelite_tf_publisher.py
+++ simulator/cso_gnc_sim/src/satelite_tf_publisher.py
@@ -2,13 +2,24 @@
 import rospy
 import tf
 import numpy as np
+import message_filters
 from geometry_msgs.msg import PoseStamped
 
 sensor_cfg = 0
-no_position = 0
+position_mode = 0
 body_frame = 0
 frame_cfg = 0
 
+parent_position = np.array([0, 0, 0])
+parent_orientation = np.array([0, 0, 0, 1])
+
+def handle_sync(pose_msg, parent_msg):
+    global parent_orientation
+    global parent_position
+    parent_orientation = np.array([parent_msg.pose.orientation.x, parent_msg.pose.orientation.y, parent_msg.pose.orientation.z, parent_msg.pose.orientation.w])
+    parent_position = np.array([parent_msg.pose.position.x*1000, parent_msg.pose.position.y*1000, parent_msg.pose.position.z*1000])
+
+    handle_pose(pose_msg)
 
 def handle_pose(msg):
     global sensor_cfg
@@ -16,14 +27,17 @@ def handle_pose(msg):
     global body_frame
     global frame_cfg
 
-    if no_position:
-        msg.pose.position.x = 0
-        msg.pose.position.y = 0
-        msg.pose.position.z = 0
+    quat = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+    pos = np.array([msg.pose.position.x*1000, msg.pose.position.y*1000, msg.pose.position.z*1000])
+
+    if position_mode == "zero_pos":
+        pos = np.array([0,0,0])
+    elif position_mode =="relative":
+        pos = pos - parent_position
 
     br = tf.TransformBroadcaster()
-    quat = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-    br.sendTransform((msg.pose.position.x*1000, msg.pose.position.y*1000, msg.pose.position.z*1000),
+
+    br.sendTransform(pos,
                      quat,
                      rospy.Time.now(),
                      body_frame,
@@ -42,7 +56,7 @@ def handle_pose(msg):
                          np.array([float(x) for x in sensor_cfg[k]["pose"].split(" ")]),
                      rospy.Time.now(),
                      k,
-                     sensor_cfg[k]["frame"])
+                     body_frame)
 
 
 if __name__ == '__main__':
@@ -51,9 +65,21 @@ if __name__ == '__main__':
     sensor_cfg =  rospy.get_param("~sensors", "")
     frame_cfg = rospy.get_param("~frames", "")
     body_frame = rospy.get_param("~body_frame", "cso")
-    no_position = bool(rospy.get_param("~no_position", True))
+    position_mode = rospy.get_param("~mode", "absolute")
 
-    rospy.Subscriber('pose',
-                     PoseStamped,
-                     handle_pose)
+    # position mode zero_pos = no position is broadcasted for body frame (for debugging)
+    # position mode absolute = absolute position from orbital elements is published
+    # position mode relative = relative position w.r.t to "relative_parent" message is published
+
+    if position_mode == "absolute" or position_mode == "zero_pos":
+        rospy.Subscriber('pose',
+                         PoseStamped,
+                         handle_pose)
+
+    elif position_mode == "relative":
+        pose_sub = message_filters.Subscriber('pose', PoseStamped)
+        relative_parent_sub = message_filters.Subscriber('relative_parent', PoseStamped)
+        ts = message_filters.TimeSynchronizer([pose_sub, relative_parent_sub], 10)
+        ts.registerCallback(handle_sync)
+
     rospy.spin()
