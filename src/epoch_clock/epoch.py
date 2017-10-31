24408d3dd75101a0717642bccaa48dd5d69333ca
Michael Pantic <michael.pantic@gmail.com>
2017-10-31 16:41:27 +0000

    Quasi-Non Singular Orbital Elements

--- src/epoch_clock/epoch.py
+++ src/epoch_clock/epoch.py
@@ -4,12 +4,11 @@ import time
 
 
 class Epoch:
-
     def __init__(self):
         i = 0
         while not rospy.has_param("/epoch") and i < 10 and not rospy.is_shutdown():
             rospy.logwarn("Epoch not avalailable yet. Waiting...")
-            i = i+1
+            i = i + 1
             time.sleep(1)
 
         self.epoch_string = rospy.get_param('/epoch')
@@ -17,7 +16,7 @@ class Epoch:
 
     def now(self):
         time_since_epoch = rospy.Time.now()
-        time_delta = timedelta(0, time_since_epoch.secs, time_since_epoch.nsecs/1e3)
+        time_delta = timedelta(0, time_since_epoch.secs, time_since_epoch.nsecs / 1e3)
         return self.epoch_datetime + time_delta
 
     def now_jd(self):
