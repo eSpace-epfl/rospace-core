fe183ba373ce15aecacf5377e498da59762557f9
Christian Lanegger <clanegge@ethz.ch>
2017-11-24 21:29:48 +0100

    added clock to orekit propagator node. Node is also running a service. This service stops the simulation when called by client. An rqt plugin was already created which calls the service. (Client not yet perfect though). Also Cartesian.py was changed (with Michael when we were bug hunting..)

--- scripts/epoch_clock.py
+++ scripts/epoch_clock.py
@@ -50,73 +50,74 @@ class EpochClockGUI(threading.Thread):
 
 
 if __name__ == '__main__':
-    rospy.init_node('epoch_clock', anonymous=True)
-    rospy.loginfo("Starting Epoch Clock with settings:")
+        rospy.init_node('epoch_clock', anonymous=True)
+        rospy.loginfo("Starting Epoch Clock with settings:")
 
-    epoch = ""
-    datetime_epoch = []
-    realtime_factor = []
-    frequency = []
-    paused = False
+        epoch = ""
+        datetime_epoch = []
+        realtime_factor = []
+        frequency = []
+        paused = False
 
-    if rospy.has_param("~init_epoch"):
-        epoch = str(rospy.get_param("~init_epoch"))
-        datetime_epoch = datetime.strptime(epoch, "%Y%m%dT%H:%M:%S")
-    else:
-        datetime_epoch = datetime.utcnow()
+        if rospy.has_param("~init_epoch"):
+            epoch = str(rospy.get_param("~init_epoch"))
+            datetime_epoch = datetime.strptime(epoch, "%Y%m%dT%H:%M:%S")
+        else:
+            datetime_epoch = datetime.utcnow()
 
-    if rospy.has_param("~realtime_factor"):
-        realtime_factor = int(rospy.get_param("~realtime_factor"))
-    else:
-        realtime_factor = 1
+        if rospy.has_param("~realtime_factor"):
+            realtime_factor = int(rospy.get_param("~realtime_factor"))
+        else:
+            realtime_factor = 1
 
-    if rospy.has_param("~frequency"):
-        frequency = int(rospy.get_param("~frequency"))
-    else:
-        frequency = 50.0
+        if rospy.has_param("~frequency"):
+            frequency = int(rospy.get_param("~frequency"))
+        else:
+            frequency = 50.0
 
-    rate = float(1) / float(frequency)
+        rate = float(1) / float(frequency)
 
-    rospy.loginfo("Epoch = " + datetime_epoch.strftime("%Y-%m-%d %H:%M:%S"))
-    rospy.loginfo("Realtime Factor = " + str(realtime_factor))
+        rospy.loginfo("Epoch = " + datetime_epoch.strftime("%Y-%m-%d %H:%M:%S"))
+        rospy.loginfo("Realtime Factor = " + str(realtime_factor))
 
-    datetime_startup = datetime.utcnow()  # this is considered time 0
+        datetime_startup = datetime.utcnow()  # this is considered time 0
 
-    rospy.set_param('/epoch', datetime_epoch.strftime("%Y-%m-%d %H:%M:%S"))
-    rospy.set_param('use_sim_time', True)
+        rospy.set_param('/epoch', datetime_epoch.strftime("%Y-%m-%d %H:%M:%S"))
+        rospy.set_param('use_sim_time', True)
 
-    # Init publisher and rate limiter
-    pub = rospy.Publisher('clock', Clock, queue_size=10)
 
-    gui = EpochClockGUI()
-    pause_start = None
+        # Init publisher and rate limiter
+        pub = rospy.Publisher('clock', Clock, queue_size=10)
 
-    # publish clock message according to realtime factor
-    while not rospy.is_shutdown():
-        if not gui.paused:
+        gui = EpochClockGUI()
+        pause_start = None
 
-            # check if we are returning from a pause
-            if pause_start is not None:
-                # correct startup time by pause time
-                datetime_startup = datetime_startup + (datetime.utcnow() - pause_start)
-                pause_start = None
+        # publish clock message according to realtime factor
+        while not rospy.is_shutdown():
+            if not gui.paused:
 
-            elapsed_time = (datetime.utcnow() - datetime_startup)
-            sim_elapsed_time = (elapsed_time * realtime_factor)
+                # check if we are returning from a pause
+                if pause_start is not None:
+                    # correct startup time by pause time
+                    datetime_startup = datetime_startup + (datetime.utcnow() - pause_start)
+                    pause_start = None
 
-            msg = Clock()
-            msg.clock.secs = int(sim_elapsed_time.seconds + sim_elapsed_time.days * (24 * 3600))
-            msg.clock.nsecs = sim_elapsed_time.microseconds * 1e3
-            pub.publish(msg)
+                elapsed_time = (datetime.utcnow() - datetime_startup)
+                sim_elapsed_time = (elapsed_time * realtime_factor)
 
-            gui.update_current_time(datetime_epoch,
-                                    sim_elapsed_time,
-                                    datetime_epoch + sim_elapsed_time)
-        if gui.paused:
-            # if we just got into a pause, store time
-            if pause_start is None:
-                pause_start = datetime.utcnow()
+                msg = Clock()
+                msg.clock.secs = int(sim_elapsed_time.seconds + sim_elapsed_time.days * (24 * 3600))
+                msg.clock.nsecs = sim_elapsed_time.microseconds * 1e3
+                pub.publish(msg)
 
-        sleep(rate)
+                gui.update_current_time(datetime_epoch,
+                                        sim_elapsed_time,
+                                        datetime_epoch + sim_elapsed_time)
+            if gui.paused:
+                # if we just got into a pause, store time
+                if pause_start is None:
+                    pause_start = datetime.utcnow()
 
-    gui.callback_quit()
+            sleep(rate)
+
+        gui.callback_quit()
