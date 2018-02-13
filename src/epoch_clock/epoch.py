3936470f679aa33c3484949bfa2077db394e75d8
Christian Lanegger <clanegge@student.ethz.ch>
2018-02-13 15:17:01 +0100

    Pre clean up commit. In case I should delete something I would need later on..
    Signed-off-by: Christian Lanegger <clanegge@student.ethz.ch>

--- src/epoch_clock/epoch.py
+++ src/epoch_clock/epoch.py
@@ -56,7 +56,9 @@ class Epoch:
     def changeFrequency(self, new_frequency):
         # Only propagator node should change this! Do not change!
         Epoch.publish_frequency = new_frequency
+        rospy.set_param('/publish_freq', new_frequency)
 
     def changeStep(self, new_step):
         # Only propagator node should change this! Do not change!
         Epoch.time_step_size = new_step
+        rospy.set_param('/time_step_size', new_step)
