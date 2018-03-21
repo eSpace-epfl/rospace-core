b4bc3b03aef3cf74d2fe93a209acf4f0bcf59f1b
michael.pantic <mpantic@student.ethz.ch>
2018-03-21 17:44:35 +0100

    Finished renaming rospace_lib
    Signed-off-by: michael.pantic <mpantic@student.ethz.ch>

--- rospace_lib/setup.py
+++ rospace_lib/setup.py
@@ -13,7 +13,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
-    packages=['space_tf'],
+    packages=['rospace_lib'],
     package_dir={'': 'src'}
 )
 
