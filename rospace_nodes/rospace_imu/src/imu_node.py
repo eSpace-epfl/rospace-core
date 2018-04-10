aa3e1c770aef436583d5d9f6eec67a9047fbacb1
michael.pantic <mpantic@student.ethz.ch>
2018-04-10 20:50:39 +0200

    Subscriber for Pose
    Signed-off-by: michael.pantic <mpantic@student.ethz.ch>

--- rospace_nodes/rospace_imu/src/imu_node.py
+++ rospace_nodes/rospace_imu/src/imu_node.py
@@ -8,14 +8,23 @@
 # for complete details.
 import rospy
 from sensor_msgs.msg import Imu
+from rospace_msgs.msg import PoseVelocityStamped
 from ADXRS614 import *
+
+def callback_pose(data):
+    rotation_rate = np.zeros(3)
+    rotation_rate[0] = data.spin.x
+    rotation_rate[1] = data.spin.y
+    rotation_rate[2] = data.spin.z
+
+    rate_gyro.set_true_value(rotation_rate)
 
 def publishIMUMessage():
     pub = rospy.Publisher('imu', Imu, queue_size=10)
-    rospy.init_node('imu_node', anonymous=True)
+
     rate = rospy.Rate(1) # 10hz
 
-    rate_gyro = ThreeAxisADXRS614()
+
 
     while not rospy.is_shutdown():
         rate_gyro_reading = np.rad2deg(rate_gyro.get_value())
@@ -32,6 +41,9 @@ def publishIMUMessage():
 
 if __name__ == '__main__':
     try:
+        rate_gyro = ThreeAxisADXRS614()
+        rospy.init_node('imu_node', anonymous=True)
+        subs = rospy.Subscriber("pose", PoseVelocityStamped, callback_pose)
         publishIMUMessage()
     except rospy.ROSInterruptException:
         pass
