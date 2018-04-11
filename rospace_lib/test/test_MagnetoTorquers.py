2147ded20b6eb286d3afb09f1716fbd4b0622f35
michael.pantic <mpantic@student.ethz.ch>
2018-04-11 21:39:07 +0200

    Added standard test (to test magnitude of magnetic dipole)
    Signed-off-by: michael.pantic <mpantic@student.ethz.ch>

--- rospace_lib/test/test_MagnetoTorquers.py
+++ rospace_lib/test/test_MagnetoTorquers.py
@@ -22,13 +22,19 @@ class MagnetoTorquersTest(unittest.TestCase):
 
     def test_dipole_moment(self):
         torquer = OneAxisMagnetoTorquer()
-        B_field = np.array([10,0, 0])
-        I = 22.7*10**-3
+        torquer._orientation = np.array([1, 0, 0]) # Torquer aligned with x-axis
+        B_field = np.array([0,1, 0]) # unit b-field in y
+        I = 22.7*10**-3  # 95% current (22.7 mA)
 
         torquer.set_B_field(B_field)
         torquer.set_I(I)
 
-        print torquer.get_torque()
+        # this should yield roughly 51.8 mNm magnetic moment around z
+        resulting_torque = torquer.get_torque()
+        self.assertAlmostEqual(51.8*10**-3, resulting_torque[2], 3)
+        # and zero for the other axes
+        self.assertAlmostEqual(0,resulting_torque[0], 6)
+        self.assertAlmostEqual(0, resulting_torque[1], 6)
 
 
 
