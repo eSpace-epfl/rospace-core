fdc1d1ed15db41ce5c7296cae4269213abe502ff
Davide Frey <frey.davide.ae@gmail.com>
2018-04-28 10:00:54 +0200

    Added __init__ files to propagator to be able to import it.

---
+++ rospace_lib/src/rospace_lib/propagator/__init__.py
@@ -0,0 +1,9 @@
+# @copyright Copyright (c) 2017, Davide Frey (frey.davide.ae@gmail.com)
+#
+# @license zlib license
+#
+# This file is licensed under the terms of the zlib license.
+# See the LICENSE.md file in the root of this repository
+# for complete details.
+
+from rospace_lib.propagator.Propagator import Propagator
